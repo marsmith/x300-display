601f6d281673c5b11e7b8de862c7dd0686ed6f89
Marty <marsmith@usgs.gov>
2018-12-05 23:21:50 -0500

    update

---
+++ dbInfo.js
@@ -0,0 +1,11 @@
+exports.credentials  = {
+    host: 'localhost',
+    user: 'hawdis',
+    password: 'abc123',
+    database: 'x300'
+};
+
+exports.database = {
+   name: 'x300',
+   table: 'logdata'
+};
